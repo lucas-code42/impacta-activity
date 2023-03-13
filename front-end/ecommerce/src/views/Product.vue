1e2613460a448ec73b71ddd5f097f49280fb7341
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-03-12 21:27:00 -0300

    inital commit

---
+++ front-end/ecommerce/src/views/Product.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+        this is the Product Page
+        
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'Product',
+
+    methods: {
+
+    },
+
+}
+</script>
+  
+<style lang="scss"></style>
+  
